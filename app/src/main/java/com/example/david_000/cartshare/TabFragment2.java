cd591731427ef94a52e0bbb0c3fefa723b319029
Justin Ngo <david_ngo_seven@yahoo.com>
2016-03-08 23:41:03 -0800

    Gallery has been added but is hard-coded atm.

--- app/src/main/java/com/example/david_000/cartshare/TabFragment2.java
+++ app/src/main/java/com/example/david_000/cartshare/TabFragment2.java
@@ -1,5 +1,7 @@
 package com.example.david_000.cartshare;
 
+import android.content.Context;
+import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -7,20 +9,77 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.Gallery;
+import android.widget.ImageView;
 
 /**
  * Created by david_000 on 1/20/2016.
  */
-public class TabFragment2 extends Fragment {
+public class TabFragment2 extends Fragment
+{
+    //the images to display
+    Integer[] imageIDs = {  R.drawable.addfriend, R.drawable.addfriend2, R.drawable.addlist, R.drawable.delete  };
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.tab2_layout, container, false);
-    }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        final View rootView = inflater.inflate(R.layout.tab2_layout, container, false);
+        //final Context ctx = getActivity().getApplicationContext();
+
+        Gallery gallery = (Gallery) rootView.findViewById(R.id.gallery1);
+        gallery.setAdapter(new ImageAdapter(getActivity()));
+        gallery.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position,long id)
+            {
+                //Toast.makeText(ctx, "pic" + (position + 1) + " selected", Toast.LENGTH_SHORT).show();
+                // display the images selected
+                ImageView imageView = (ImageView) rootView.findViewById(R.id.image1);
+                imageView.setImageResource(imageIDs[position]);
+            }
+        });
+
+        return rootView;
+    }  //end onCreateView
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         // TODO Add your menu entries here
         super.onCreateOptionsMenu(menu, inflater);
     }
-}
+
+    public class ImageAdapter extends BaseAdapter
+    {
+        private Context context;
+        private int itemBackground;
+        public ImageAdapter(Context c)
+        {
+            context = c;
+            // sets a grey background; wraps around the images
+            TypedArray a = c.obtainStyledAttributes(R.styleable.MyGallery);
+            itemBackground = a.getResourceId(R.styleable.MyGallery_android_galleryItemBackground, 0);
+            a.recycle();
+        }
+        // returns the number of images
+        public int getCount() {
+            return imageIDs.length;
+        }
+        // returns the ID of an item
+        public Object getItem(int position) {
+            return position;
+        }
+        // returns the ID of an item
+        public long getItemId(int position) {
+            return position;
+        }
+        // returns an ImageView view
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView imageView = new ImageView(context);
+            imageView.setImageResource(imageIDs[position]);
+            imageView.setLayoutParams(new Gallery.LayoutParams(100, 100));
+            imageView.setBackgroundResource(itemBackground);
+            return imageView;
+        }
+    }  //end ImageAdapter
+}  //end TabFragment2
